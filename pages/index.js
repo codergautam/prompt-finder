8c1775a7bde9f76b4a1fb808fb17172b9cc59a24
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2023-08-10 17:21:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-800">AIPrompts</h1>
+      <h1 className="text-4xl font-bold text-blue-800">Prompt Finder</h1>
     </div>
   )
 }
