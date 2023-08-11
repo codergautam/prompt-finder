91db1c55d55debc5cb83b994f370f4ad8d348129
Coder Gautam <gautamgxtv@gmail.com>
2023-08-11 11:26:05 -0500

    Commit config

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+  "SUPABASE_URL": "https://dlnxornisxegvbhecglg.supabase.co",
+  "SUPABASE_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsbnhvcm5pc3hlZ3ZiaGVjZ2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3NjA1MjksImV4cCI6MjAwNzMzNjUyOX0.vHx2Nx-xVUzL9ZjO-TqgAfRz8rhWK7fZkwCN6zGItoI"
+}
+
+export default config;
