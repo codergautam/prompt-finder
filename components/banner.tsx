472c4d9107554de9abb52e46de1d8c9b78ba8f76
Gautam Anand <gautamgxtv@gmail.com>
2023-09-04 11:02:29 -0500

    Test

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,7 +8,7 @@ export default function Banner() {
 
   return (
     <>
-      {bannerOpen && (
+      {/* {bannerOpen && (
         <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
           <div className="bg-slate-800 text-slate-50 text-sm p-3 md:rounded shadow-lg flex justify-between">
             <div className='text-slate-500 inline-flex'><a className="font-medium hover:underline text-slate-50" href="https://github.com/cruip/tailwind-landing-page-template" target="_blank" rel="noreferrer">Download<span className="hidden sm:inline"> on GitHub</span></a> <span className="italic px-1.5">or</span> <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/simple/" target="_blank" rel="noreferrer">Check Premium Version</a></div>
@@ -20,7 +20,7 @@ export default function Banner() {
             </button>
           </div>
         </div>
-      )}
+      )} */}
     </>
   )
 }
--- components/modal-video.tsx
+++ components/modal-video.tsx
@@ -64,13 +64,13 @@ export default function ModalVideo({
               </g>
             </svg>
           </div>
-          <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => { setModalOpen(true) }}>
+          {/* <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={() => { setModalOpen(true) }}>
             <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
               <path d="M10 17l6-5-6-5z" />
             </svg>
             <span className="ml-3">Watch the full video (2 min)</span>
-          </button>
+          </button> */}
         </div>
       </div>
       {/* End: Video thumbnail */}
