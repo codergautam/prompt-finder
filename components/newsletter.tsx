2fdd5b0e3bf25d387299af666d4b877928eddf39
Gautam Anand <gautamgxtv@gmail.com>
2023-09-07 06:35:27 -0500

    Basic stripe

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -37,18 +37,18 @@ export default function Newsletter() {
 
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
-                <h3 className="h3 text-white mb-2">Want more tutorials & guides?</h3>
-                <p className="text-gray-300 text-lg mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit nemo expedita voluptas culpa sapiente.</p>
+                <h3 className="h3 text-white mb-2">Want a custom prompt?</h3>
+                <p className="text-gray-300 text-lg mb-6">You can now order a custom prompt for your custom use. </p>
 
                 {/* CTA form */}
-                <form className="w-full lg:w-auto">
-                  <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
-                    <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" />
-                    <a className="btn text-white bg-blue-600 hover:bg-blue-700 shadow" href="#0">Subscribe</a>
+                <form className="w-full lg:w-auto" action="/api/buy" method="POST">
+                  <div className="flex flex-col sm:flex-row max-w-xs mx-auto sm:max-w-md lg:mx-0">
+                    {/* <input type="email" className="form-input w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500" placeholder="Your email…" aria-label="Your email…" /> */}
+                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 shadow">Order for $10</button>
                   </div>
                   {/* Success message */}
                   {/* <p className="text-sm text-gray-400 mt-3">Thanks for subscribing!</p> */}
-                  <p className="text-sm text-gray-400 mt-3">No spam. You can unsubscribe at any time.</p>
+                  <p className="text-sm text-gray-400 mt-3">Prompts are built to order and delivered within 7 days.</p>
                 </form>
               </div>
 
