90e40a08009a2ca99d4b20b4219cd376dd897d82
Gautam Anand <gautamgxtv@gmail.com>
2023-09-06 22:07:15 -0500

    Remove header

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-          <Header />
+          {/* <Header /> */}
           {children}
           <Banner />
         </div>
